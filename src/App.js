2d7e4c85ceb5351c9d2a278b6079960c4a52c0cb
jonah wagner <jonahwagner231@gmail.com>
2023-03-21 03:48:04 -0700

    setup fetch to grab user profile from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Metronome from "./components/metronome/Metronome";
 import Header from "./components/Header";
 
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 import { DarkModeContext } from "./context/DarkModeContext";
 
 export default function App() {
+  useEffect(() => {
+    // fetch profile details from express api
+    (async () => {
+      try {
+        const response = await fetch("/user/profile");
+        const user = await response.json();
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+  // set body element to match theme
   const { darkMode } = useContext(DarkModeContext);
   const theme = darkMode ? "text-bg-dark" : "text-bg-light";
   document.body.className = theme + " overflow-hidden";
